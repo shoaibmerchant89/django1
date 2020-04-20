f2fe920a3d5202bde27440661c0f1cb52bc86023
shoaibmerchant89 <shoaibmerchant89@gmail.com>
2020-04-21 03:48:01 +0530

    Section 5 Completed

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.http import HttpResponse
+from pages import views
+
 def index(request):
-    return render(request, 'pages/index.html')
+    return render(request, 'pages/index.html', status=200)
 
 def about(request):
     return render(request, 'pages/about.html')
